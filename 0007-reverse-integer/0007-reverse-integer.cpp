0899b5315e2740345019997bdac0b14a03118288
malak shahin <121239888+MalakShahin@users.noreply.github.com>
2025-09-09 22:33:39 -0700

    Time: 0 ms (100%), Space: 8.6 MB (20.29%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,12 +6,10 @@ public:
         while (x != 0) {
             int digit = x % 10;
             
-            // Overflow check
-            // For positive numbers
             if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7)) {
                 return 0;
             }
-            // For negative numbers
+            
             if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8)) {
                 return 0;
             }
